ef5083bd1006ec8be01ec39df5f1def90d696622
Sujit <85719346+sujit-bizcloudexperts@users.noreply.github.com>
2022-07-15 19:46:12 +0530

    modification in shipment report sent condition

--- src/shared/filterReportData/index.js
+++ src/shared/filterReportData/index.js
@@ -12,17 +12,31 @@ async function filterReportData(result, tableName) {
             result[key]['shipment datetime'] = JSON.stringify(result[key]['shipment datetime']);
             result[key]['shipment datetime'] = `${(result[key]['shipment datetime']).split('T')[0]}"`
             if (result[key]['order status'] == "DEL") {
-
                 let shipment = await scanTableData(tableName, result[key]['file_nbr']);
-                if (!shipment.length) {
-                    let row = result[key];
-                    reportData.push(row);
-                    let rowObj = {
-                        PutRequest: {
-                            Item: row
+                if (shipment.length) {
+                    if(shipment[0].sent_count != 3){
+                        let row = result[key];
+                        reportData.push(row);
+                        result[key]['sent_count'] = shipment[0].sent_count + 1
+                        let updateSentCount = resultData[key]
+                        let rowObj = {
+                            PutRequest: {
+                                Item: updateSentCount
+                            }
                         }
+                        dbRows.push(rowObj);
                     }
-                    dbRows.push(rowObj);
+                }else {
+                        let row = result[key];
+                        reportData.push(row);
+                        result[key]['sent_count'] = 0
+                        let updateSentCount = resultData[key]
+                        let rowObj = {
+                            PutRequest: {
+                                Item: updateSentCount
+                            }
+                        }
+                        dbRows.push(rowObj);
                 }
             } else {
                 reportData.push(result[key]);
